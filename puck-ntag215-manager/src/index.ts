0022561ba2f19194b0734cbf6f5f5347a3ff56ed
Daniel <790119+DanTheMan827@users.noreply.github.com>
2022-08-25 22:42:50 -0500

    Clear with random tag

--- puck-ntag215-manager/src/index.ts
+++ puck-ntag215-manager/src/index.ts
@@ -5,7 +5,6 @@ import { showModal, hideModal, setModal } from "./modal"
 import { saveData, readFile } from "./fileHelpers"
 
 const anyWindow = (window as any)
-const blankTag = new Uint8Array(require('arraybuffer-loader!../NTAG215_blank.bin'))
 const puck = anyWindow.puck = new Puck(console.log, console.warn, console.error)
 
 $(() => {
@@ -99,7 +98,21 @@ $(() => {
     element.find("a.slot-clear-link").on("click", async (e) => {
       e.preventDefault()
 
-      await writeSlot(slot, blankTag, element)
+      var tag = new Uint8Array(572)
+      tag[0] = 0x04
+      tag[1] = Math.round(Math.random() * 255)
+      tag[2] = Math.round(Math.random() * 255)
+      tag[3] = tag[0] ^ tag[1] ^ tag[2] ^ 0x88
+      tag[4] = Math.round(Math.random() * 255)
+      tag[5] = Math.round(Math.random() * 255)
+      tag[6] = Math.round(Math.random() * 255)
+      tag[7] = Math.round(Math.random() * 255)
+      tag[8] = tag[4] ^ tag[5] ^ tag[6] ^ tag[7]
+
+      tag.set([0x48, 0x00, 0x00, 0xE1, 0x10, 0x3E, 0x00, 0x03, 0x00, 0xFE], 0x09)
+      tag.set([0xBD, 0x04, 0x00, 0x00, 0xFF, 0x00, 0x05], 0x20B)
+
+      await writeSlot(slot, tag, element)
     })
 
     element.find("a.slot-select-link").on("click", async (e) => {
