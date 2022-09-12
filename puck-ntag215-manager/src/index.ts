779027d4e0745007c8ebb44cf702649ce314cefe
Daniel <790119+DanTheMan827@users.noreply.github.com>
2022-09-11 19:25:24 -0500

    Move NTAG215 creation into separate file

--- puck-ntag215-manager/src/index.ts
+++ puck-ntag215-manager/src/index.ts
@@ -1,5 +1,6 @@
 require("./style/main.scss")
 
+import { getBlankNtag } from "./ntag215"
 import { Puck } from "./puck"
 import { showModal, hideModal, setModal } from "./modal"
 import { saveData, readFile } from "./fileHelpers"
@@ -91,28 +92,14 @@ $(() => {
           await writeSlot(slot, file.data, element)
         } catch (error) {
           await showModal("Error", error)
-        }
+        }``
       }, 572)
     })
 
     element.find("a.slot-clear-link").on("click", async (e) => {
       e.preventDefault()
 
-      var tag = new Uint8Array(572)
-      tag[0] = 0x04
-      tag[1] = Math.round(Math.random() * 255)
-      tag[2] = Math.round(Math.random() * 255)
-      tag[3] = tag[0] ^ tag[1] ^ tag[2] ^ 0x88
-      tag[4] = Math.round(Math.random() * 255)
-      tag[5] = Math.round(Math.random() * 255)
-      tag[6] = Math.round(Math.random() * 255)
-      tag[7] = Math.round(Math.random() * 255)
-      tag[8] = tag[4] ^ tag[5] ^ tag[6] ^ tag[7]
-
-      tag.set([0x48, 0x00, 0x00, 0xE1, 0x10, 0x3E, 0x00, 0x03, 0x00, 0xFE], 0x09)
-      tag.set([0xBD, 0x04, 0x00, 0x00, 0xFF, 0x00, 0x05], 0x20B)
-
-      await writeSlot(slot, tag, element)
+      await writeSlot(slot, getBlankNtag(), element)
     })
 
     element.find("a.slot-select-link").on("click", async (e) => {
---
+++ puck-ntag215-manager/src/ntag215.ts
@@ -0,0 +1,21 @@
+/**
+ * This function generates and returns a 572-byte array populated with the data of a blank NTAG215 with a random UID.
+ * @returns An array with blank NTAG215 data.
+ */
+export function getBlankNtag() {
+  var tag = new Uint8Array(572)
+  tag[0] = 0x04
+  tag[1] = Math.round(Math.random() * 255)
+  tag[2] = Math.round(Math.random() * 255)
+  tag[3] = tag[0] ^ tag[1] ^ tag[2] ^ 0x88
+  tag[4] = Math.round(Math.random() * 255)
+  tag[5] = Math.round(Math.random() * 255)
+  tag[6] = Math.round(Math.random() * 255)
+  tag[7] = Math.round(Math.random() * 255)
+  tag[8] = tag[4] ^ tag[5] ^ tag[6] ^ tag[7]
+
+  tag.set([0x48, 0x00, 0x00, 0xE1, 0x10, 0x3E, 0x00, 0x03, 0x00, 0xFE], 0x09)
+  tag.set([0xBD, 0x04, 0x00, 0x00, 0xFF, 0x00, 0x05], 0x20B)
+
+  return tag;
+}
